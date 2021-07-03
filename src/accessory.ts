c666fec570b5eb2cc56ab4ed592bd723d2a1f8f3
Chaldron <aditya.mansha@gmail.com>
2021-07-03 15:07:38 -0500

    Add initial accessory plugin

--- src/accessory.ts
+++ src/accessory.ts
@@ -11,86 +11,103 @@ import {
   Service
 } from "homebridge";
 
-/*
- * IMPORTANT NOTICE
- *
- * One thing you need to take care of is, that you never ever ever import anything directly from the "homebridge" module (or the "hap-nodejs" module).
- * The above import block may seem like, that we do exactly that, but actually those imports are only used for types and interfaces
- * and will disappear once the code is compiled to Javascript.
- * In fact you can check that by running `npm run build` and opening the compiled Javascript file in the `dist` folder.
- * You will notice that the file does not contain a `... = require("homebridge");` statement anywhere in the code.
- *
- * The contents of the above import statement MUST ONLY be used for type annotation or accessing things like CONST ENUMS,
- * which is a special case as they get replaced by the actual value and do not remain as a reference in the compiled code.
- * Meaning normal enums are bad, const enums can be used.
- *
- * You MUST NOT import anything else which remains as a reference in the code, as this will result in
- * a `... = require("homebridge");` to be compiled into the final Javascript code.
- * This typically leads to unexpected behavior at runtime, as in many cases it won't be able to find the module
- * or will import another instance of homebridge causing collisions.
- *
- * To mitigate this the {@link API | Homebridge API} exposes the whole suite of HAP-NodeJS inside the `hap` property
- * of the api object, which can be acquired for example in the initializer function. This reference can be stored
- * like this for example and used to access all exported variables and classes from HAP-NodeJS.
- */
+import axios from 'axios';
+
 let hap: HAP;
 
-/*
- * Initializer function called when the plugin is loaded.
- */
 export = (api: API) => {
   hap = api.hap;
-  api.registerAccessory("ExampleSwitch", ExampleSwitch);
+  api.registerAccessory("Harbor Breeze Fan", HomeBreezeFan);
 };
 
-class ExampleSwitch implements AccessoryPlugin {
+class Firmware {
+  private readonly BaseURL: String = "http://hbfan/";
+  private readonly log: Logging;
+
+  constructor(log: Logging) {
+    this.log = log;
+  }
+
+  identify(): Promise<any> {
+    this.log.info("Identifying");
+    return axios.get(this.BaseURL + "identify");
+  }
+
+  getActive(): Promise<any> {
+    this.log.info("Getting active state");
+    return axios.get(this.BaseURL + "active").then(r => r.data).then(r => r['active']);
+  };
+
+  getRotationSpeed(): Promise<any> {
+    this.log.info("Getting rotation speed");
+    return axios.get(this.BaseURL + "speed").then(r => r.data).then(r => r['speed']);
+  };
+
+  setActive(active: boolean) {
+    this.log.info("Setting active to " + active);
+    axios.get(this.BaseURL + "set_active?param=" + (active ? "1" : "0"));
+  };
+
+  setSpeed(speed: number) {
+    this.log.info("Setting speed to " + speed);
+    axios.get(this.BaseURL + "set_speed?param=" + speed.toString());
+  };
+}
+
+class HomeBreezeFan implements AccessoryPlugin {
+
 
   private readonly log: Logging;
   private readonly name: string;
-  private switchOn = false;
-
-  private readonly switchService: Service;
+  private readonly api: Firmware;
+  private readonly fanService: Service;
   private readonly informationService: Service;
+
 
   constructor(log: Logging, config: AccessoryConfig, api: API) {
     this.log = log;
-    this.name = config.name;
 
-    this.switchService = new hap.Service.Switch(this.name);
-    this.switchService.getCharacteristic(hap.Characteristic.On)
+    this.name = config.name;
+    this.api = new Firmware(log);
+
+    this.fanService = new hap.Service.Fanv2(this.name);
+    this.fanService.getCharacteristic(hap.Characteristic.Active)
       .on(CharacteristicEventTypes.GET, (callback: CharacteristicGetCallback) => {
-        log.info("Current state of the switch was returned: " + (this.switchOn? "ON": "OFF"));
-        callback(undefined, this.switchOn);
+        this.api.getActive().then(active => callback(undefined, active));
       })
       .on(CharacteristicEventTypes.SET, (value: CharacteristicValue, callback: CharacteristicSetCallback) => {
-        this.switchOn = value as boolean;
-        log.info("Switch state was set to: " + (this.switchOn? "ON": "OFF"));
+        let active: boolean = value as boolean;
+        this.api.setActive(active);
+        callback();
+      });
+    this.fanService.getCharacteristic(hap.Characteristic.RotationSpeed)
+      .setProps({ minValue: 0, maxValue: 3, minStep: 1 })
+      .on(CharacteristicEventTypes.GET, (callback: CharacteristicGetCallback) => {
+        this.api.getRotationSpeed().then(speed => callback(undefined, speed));
+      })
+      .on(CharacteristicEventTypes.SET, (value: CharacteristicValue, callback: CharacteristicSetCallback) => {
+        let speed: number = value as number;
+        this.api.setSpeed(speed);
         callback();
       });
 
     this.informationService = new hap.Service.AccessoryInformation()
-      .setCharacteristic(hap.Characteristic.Manufacturer, "Custom Manufacturer")
-      .setCharacteristic(hap.Characteristic.Model, "Custom Model");
+      .setCharacteristic(hap.Characteristic.Manufacturer, "Harbor Breeze")
+      .setCharacteristic(hap.Characteristic.Model, "CHQ7030T")
+      .setCharacteristic(hap.Characteristic.SerialNumber, "1289")
+      .setCharacteristic(hap.Characteristic.FirmwareRevision, "1.1");
 
-    log.info("Switch finished initializing!");
+    log.info("Finished initializing!");
   }
 
-  /*
-   * This method is optional to implement. It is called when HomeKit ask to identify the accessory.
-   * Typical this only ever happens at the pairing process.
-   */
   identify(): void {
-    this.log("Identify!");
+    this.api.identify();
   }
 
-  /*
-   * This method is called directly after creation of this instance.
-   * It should return all services which should be added to the accessory.
-   */
   getServices(): Service[] {
     return [
       this.informationService,
-      this.switchService,
+      this.fanService,
     ];
   }
 
